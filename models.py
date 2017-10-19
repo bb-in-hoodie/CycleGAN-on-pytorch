8eb5a2c90889b75fa696f74c3718a7509101107c
BlackBindy <pso999@naver.com>
2017-10-19 20:31:14 +0900

    create train.py file

--- models.py
+++ models.py
@@ -1,10 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
-import torchvision
-import torchvision.transforms as transforms
-from torch.autograd import Variable
 
 channel_size = 3
 
