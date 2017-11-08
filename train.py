04598aaeea2510ddf0da5fe091f6150128c02ccf
blackbindy <pso999@naver.com>
2017-11-09 01:31:17 +0900

    lr_g: 0.0002, lr_d: 0.00003, cc_lambda: 6

--- train.py
+++ train.py
@@ -19,9 +19,9 @@ start_spurt_num = 15 # save images on the first n checkpoints
 # Hyperparameters
 lr_G = 0.0002 # (0.0002 on the paper)
 lr_D = 0.00003 # (0.0001 on the paper)
-cc_lambda = 10 # lambda of cycle-consistency loss (10 on the paper)
+cc_lambda = 6 # lambda of cycle-consistency loss (10 on the paper)
 batch_size = 1
-total_epoch = 100
+total_epoch = 10
 
 # Initial time
 init_time = time.time()
